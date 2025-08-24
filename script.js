d15ca6852e3ea3881b393cc9d45e8e09cd3a555f
jdosk <jdoskocil86@gmail.com>
2025-08-24 11:04:23 -0700

    Most functionality in place, all math functions generally working, now dealing with minor bug fixes and error handling

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const buttons = document.querySelectorAll(".btn");
 const display = document.querySelector(".display");
 
-const numberOne = "";
-const numberTwo = "";
 const maxDisplay = 14;
+let numberOne;
+let numberTwo;
+let currentOperator;
+let waitingToOperate = false;
 
 const clearDisplay = function() {
     display.textContent = "0";
@@ -23,7 +25,6 @@ const clearZeros = function() {
 };
 
 const checkMaxDisplay = function() {
-    console.log(display.textContent.length === 14);
     return display.textContent.length === 14;
 };
 
@@ -38,75 +39,136 @@ const appendToDisplay = function(num) {
     }
 };
 
+const checkDecimals = function() {
+    return display.textContent.includes(".");
+}
+
+const getSecondNum = function(operator) {
+    if (!waitingToOperate) {
+        numberOne = display.textContent;
+        clearDisplay();
+        waitingToOperate = true;
+        currentOperator = operator;
+    } else {
+        numberTwo = display.textContent;
+        let result = operate(numberOne, numberTwo, currentOperator);
+        numberOne = result;
+        currentOperator = operator;
+        clearDisplay();
+    }
+}
+
+const add = function(numOne, numTwo) {
+    let x = parseFloat(numOne);
+    let y = parseFloat(numTwo);
+
+    return (x + y).toString();
+}
+
+const subtract = function(numOne, numTwo) {
+    let x = parseFloat(numOne);
+    let y = parseFloat(numTwo);
+    
+    return (x - y).toString();
+}
+
+const multiply = function(numOne, numTwo) {
+    let x = parseFloat(numOne);
+    let y = parseFloat(numTwo);
+    
+    return (x * y).toString();
+}
+
+const divide = function(numOne, numTwo) {
+    let x = parseFloat(numOne);
+    let y = parseFloat(numTwo);
+    
+    return (x / y).toString();
+}
+
+const operate = function(num1, num2, operator) {
+    switch (operator) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+    }
+}
+
+const equals = function() {
+    if (currentOperator != "") {
+        numberTwo = display.textContent;
+        let result = operate(numberOne, numberTwo, currentOperator);
+        display.textContent = result;
+        currentOperator = "";
+    }
+    
+}
+
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     switch (button.id) {
         case "one":
-            console.log("1");
             appendToDisplay("1");
             break;
         case "two":
             appendToDisplay("2");
-            console.log("2");
             break;
         case "three":
             appendToDisplay("3");
-            console.log("3");
             break;
         case "four":
             appendToDisplay("4");
-            console.log("4");
             break;
         case "five":
             appendToDisplay("5");
-            console.log("5");
             break;
         case "six":
             appendToDisplay("6");
-            console.log("6");
             break;
         case "seven":
             appendToDisplay("7");
-            console.log("7");
             break;
         case "eight":
             appendToDisplay("8");
-            console.log("8");
             break;
         case "nine":
             appendToDisplay("9");
-            console.log("9");
             break;
         case "zero":
             appendToDisplay("0");
-            console.log("0");
             break;
         case "decimal":
-            appendToDisplay(".");
-            console.log(".");
+            if (checkDecimals()) {
+                break;
+            } else appendToDisplay(".");
             break;
         case "add":
-            console.log("add");
+            getSecondNum("add");
             break;
         case "subtract":
-            console.log("subtract");
+            getSecondNum("subtract");
             break;
         case "multiply":
-            console.log("multiply");
+            getSecondNum("multiply");
             break;
         case "divide":
-            console.log("divide");
+            getSecondNum("divide");
             break;
         case "ac":
-            console.log("ac");
             clearDisplay();
+            waitingToOperate = false;
             break;
         case "del":
-            console.log("del");
             deleteCharacter();
             break;
         case "equals":
             console.log("equals");
+            equals();
             break;
         default:
             console.log("default");
